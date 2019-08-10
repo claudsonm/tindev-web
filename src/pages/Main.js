8dce04e709e3501afa8370355618efd76f71ce06
Claudson Martins <claudson@outlook.com>
2019-08-10 20:28:58 -0300

    Implementado o match real-time

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import io from 'socket.io-client';
 import './Main.css';
 import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDeveloper, setMatchDeveloper] = useState(null);
     // Every time the useEffect dependencies (inputs) are updated, run the function.
     useEffect(() => {
         // Same notation as define the function and call it on the next line.
@@ -19,6 +22,16 @@ export default function Main({ match }) {
             });
             setUsers(response.data);
         })();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDeveloper(dev);
+        });
     }, [match.params.id]);
 
     async function handleLike(id) {
@@ -66,6 +79,16 @@ export default function Main({ match }) {
                 </ul>
             ) : (
                 <div className="empty">Sem mais usuários</div>
+            )}
+
+            { matchDeveloper && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match"/>
+                    <img className="avatar" src={matchDeveloper.avatar} alt="Avatar do desenvolvedor"/>
+                    <strong>{ matchDeveloper.name }</strong>
+                    <p>{ matchDeveloper.bio }</p>
+                    <button type="button" onClick={() => setMatchDeveloper(null)}>FECHAR</button>
+                </div>
             )}
         </div>
     );
